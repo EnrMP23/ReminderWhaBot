f8231047d4738a2f28093af8966b1c70f1aff622
EnrMP23 <enrmp2002@gmail.com>
2024-12-01 21:28:09 -0700

    Update whabot.py

--- whabot.py
+++ whabot.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 API_KEY = os.getenv("SPORTSDATAIO_DATA_API_KEY", "205bca4e7d76426ea69a738d9ef11641")  # API key
 TELEGRAM_TOKEN = os.getenv("BOT_TOKEN", "7163814190:AAGzhkR3H3SLBQc4LF4Zxi3J4_RnKd26u1M")  # Reemplaza con tu token real
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://reminderwhabot-vsig.onrender.com/webhook")  # URL pública de tu webhook
-SEASON = 2024
+season = 2024
 
 # Umbrales
 confidence_threshold = 0.55
