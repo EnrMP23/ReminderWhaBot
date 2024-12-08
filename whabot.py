4ecb82131d1f1b2af4fd8e4d1fe313d50dc6ea0e
EnrMP23 <enrmp2002@gmail.com>
2024-12-07 18:16:13 -0700

    Update whabot.py

--- whabot.py
+++ whabot.py
@@ -107,7 +107,7 @@ async def monitoreo_automatico(context: CallbackContext):
 
 # Configuración del bot
 async def main():
-    token = (TELEGRAM_TOKEN)  # Reemplaza esto con tu token real del bot
+    token = "TELEGRAM_TOKEN"  # Reemplaza esto con tu token real del bot
     application = Application.builder().token(token).build()
 
     # Configura JobQueue
